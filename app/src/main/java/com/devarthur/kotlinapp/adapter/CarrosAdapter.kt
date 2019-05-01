3b5cd6cdf609d7cd508225451b7023da92ec6ed9
Arthur <arthur.gomes_4718@hotmail.com>
2019-05-01 01:01:08 -0300

    Carros Adapter

---
+++ app/src/main/java/com/devarthur/kotlinapp/adapter/CarrosAdapter.kt
@@ -0,0 +1,8 @@
+package com.devarthur.kotlinapp.adapter
+
+import com.devarthur.kotlinapp.domain.Carro
+
+//* Created by Arthur Gomes at 2019-05-01 00:56 - contact me at devarthur4718@gmail.com.br
+class CarrosAdapter {
+
+}
