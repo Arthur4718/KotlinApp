e5221035252c719de0a31c1d73fd23c68c073dde
Arthur <arthur.gomes_4718@hotmail.com>
2019-05-02 22:59:20 -0300

    CarrosFragment

--- app/src/main/java/com/devarthur/kotlinapp/fragments/CarrosFragment.kt
+++ app/src/main/java/com/devarthur/kotlinapp/fragments/CarrosFragment.kt
@@ -2,6 +2,9 @@ package com.devarthur.kotlinapp.fragments
 
 
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +13,18 @@ import android.widget.TextView
 import com.devarthur.kotlinapp.domain.tipoCarro
 
 import com.devarthur.kotlinapp.R
+import com.devarthur.kotlinapp.adapter.CarroAdapter
+import com.devarthur.kotlinapp.domain.Carro
+import com.devarthur.kotlinapp.domain.CarroService
+import kotlinx.android.synthetic.main.fragment_carros.*
+import org.jetbrains.anko.support.v4.toast
 
 private var type : tipoCarro = tipoCarro.classicos
 
 class CarrosFragment : BaseFragment() {
+
+    private var carros = listOf<Carro>()
+    var recyclerView : RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +47,33 @@ class CarrosFragment : BaseFragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Views
+        recyclerView = view?.findViewById<RecyclerView>(R.id.recyclerView)
 
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+        recyclerView?.itemAnimator = DefaultItemAnimator()
+        recyclerView?.setHasFixedSize(true)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        taskCarros()
+    }
+
+    private fun taskCarros() {
+        //Search the itens
+        this.carros = CarroService.getItens(context!!, type)
+
+        //Updates the list
+        recyclerView?.adapter = CarroAdapter(carros,
+            { carro : Carro ->
+              toast("@Clicou no item ${carro.itemName}")
+            }
+
+            )
+    }
 }
